4b571cf48f8d555ac62b2c885eb0989209ee0ff0
Olena Chepizhna <olena.chepizhna@sixt.com>
2024-12-05 17:56:49 +0200

    updated sql

--- src/main/resources/persistence/init.sql
+++ src/main/resources/persistence/init.sql
@@ -8,4 +8,4 @@ INSERT INTO items (name, description)
     VALUES  ('Item 1', 'desc123'),
             ('Item 2', 'desc456'),
             ('Item 3', 'desc789'),
-            ('Item 4', 'desc012');
+            ('Item 4', 'desc019');
